8c72e81769a6683e7596b65cbfae4c392d4a95d9
Duncan <duncan.a.rowland@gmail.com>
2017-03-28 10:03:56 +0100

    Added layers 1 and 2

--- server.py
+++ server.py
@@ -80,7 +80,7 @@ class requestHandler(tornado.web.RequestHandler):
       h = re.sub(r'[^0-9]', '', h) #sanitise
       h = -1 if h=='' else clamp(int(h),80,800)
 
-      #Date Bands
+      #Date Band (0)
       b0from = self.get_argument('b0from',default='0000')
       b0from = re.sub(r'[^0-9]', '', b0from) #sanitise
       if not len(b0from)==4: b0from=False
@@ -91,24 +91,64 @@ class requestHandler(tornado.web.RequestHandler):
       b0fill = re.sub(r'[^A-F0-9]', '', b0fill) #sanitise
       if not len(b0fill)==6: b0fill='000000'
       #b0bord =self.get_argument('b0bord',default='000000')
-      rangeurl = '' if not (b0from and b0to) else '+AND+year:['+b0from+'+TO+'+b0to+']'
+      rangeurl0 = '' if not (b0from and b0to) else '+AND+year:['+b0from+'+TO+'+b0to+']'
+
+      #Date Band (1 optional)
+      if self.get_argument('b1from',default=False) or self.get_argument('b1to',default=False):
+         b1from = self.get_argument('b1from',default='0000')
+         b1from = re.sub(r'[^0-9]', '', b1from) #sanitise
+         if not len(b1from)==4: b1from=False
+         b1to = self.get_argument('b1to',default='9999')
+         b1to = re.sub(r'[^0-9]', '', b1to) #sanitise
+         if not len(b1to)==4: b1to=False
+         b1fill = self.get_argument('b1fill',default='FF00FF').upper()
+         b1fill = re.sub(r'[^A-F0-9]', '', b1fill) #sanitise
+         if not len(b1fill)==6: b1fill='000000'
+         #b1bord =self.get_argument('b1bord',default='000000')
+         rangeurl1 = '+AND+year:['+b1from+'+TO+'+b1to+']'
+      else:
+         rangeurl1 = ''
+
+      #Date Band (2 optional)
+      if self.get_argument('b2from',default=False) or self.get_argument('b2to',default=False):
+         b2from = self.get_argument('b2from',default='0000')
+         b2from = re.sub(r'[^0-9]', '', b2from) #sanitise
+         if not len(b2from)==4: b2from=False
+         b2to = self.get_argument('b2to',default='9999')
+         b2to = re.sub(r'[^0-9]', '', b2to) #sanitise
+         if not len(b2to)==4: b2to=False
+         b2fill = self.get_argument('b2fill',default='00FFFF').upper()
+         b2fill = re.sub(r'[^A-F0-9]', '', b2fill) #sanitise
+         if not len(b2fill)==6: b2fill='000000'
+         #b2bord =self.get_argument('b2bord',default='000000')
+         rangeurl2 = '+AND+year:['+b2from+'+TO+'+b2to+']'
+      else:
+         rangeurl2 = ''
 
       dpt=400000
 
-      url1="https://layers.nbnatlas.org/geoserver/ALA/wms?layers=ALA:county_coastal_terrestrial_region"
-      url2="https://records-ws.nbnatlas.org/ogc/wms/reflect?q=*:*&fq=species_guid:"+tvk+druidurl+rangeurl+"&ENV=colourmode:osgrid;color:"+b0fill+";opacity:0.75;gridlabels:false;gridres:singlegrid"
+      urlBase="https://layers.nbnatlas.org/geoserver/ALA/wms?layers=ALA:county_coastal_terrestrial_region"
+      url0="https://records-ws.nbnatlas.org/ogc/wms/reflect?q=*:*&fq=species_guid:"+tvk+druidurl+rangeurl0+"&ENV=colourmode:osgrid;color:"+b0fill+";opacity:0.75;gridlabels:false;gridres:singlegrid"
+      url1=False if rangeurl1=='' else "https://records-ws.nbnatlas.org/ogc/wms/reflect?q=*:*&fq=species_guid:"+tvk+druidurl+rangeurl1+"&ENV=colourmode:osgrid;color:"+b1fill+";opacity:0.75;gridlabels:false;gridres:singlegrid"
+      url2=False if rangeurl2=='' else "https://records-ws.nbnatlas.org/ogc/wms/reflect?q=*:*&fq=species_guid:"+tvk+druidurl+rangeurl2+"&ENV=colourmode:osgrid;color:"+b2fill+";opacity:0.75;gridlabels:false;gridres:singlegrid"
+
       #Supersampling 
       #img1=imageFor(url1, lon0, lat0, lon1, lat1, w*2, h*2, dpt)
       #img1.thumbnail((img1.size[0]/2,img1.size[1]/2), Image.LINEAR)
       #img2=imageFor(url2, lon0, lat0, lon1, lat1, w*2, h*2, dpt)
       #img2.thumbnail((img2.size[0]/2,img2.size[1]/2), Image.LINEAR)
-      img1=imageFor(url1, lon0, lat0, lon1, lat1, w, h, dpt)
-      gray = img1.convert('L')
-      img1 = gray.point(lambda x: 0 if x<8 else 255, 'L')
-      img1 = img1.convert('RGBA')
-      img2=imageFor(url2, lon0, lat0, lon1, lat1, w, h, dpt)
-      img3=Image.alpha_composite(img1,img2)
-      img3.save( 'tmp.png', 'PNG' )
+      imgBase = imageFor(urlBase, lon0, lat0, lon1, lat1, w, h, dpt)
+      imgBaseGreyThreshold = imgBase.convert('L').point(lambda x: 0 if x<8 else 255, 'L')
+      imgBase = imgBaseGreyThreshold.convert('RGBA')
+      imgLayer=imageFor(url0, lon0, lat0, lon1, lat1, w, h, dpt)
+      imgResult=Image.alpha_composite(imgBase,imgLayer)
+      if url1:
+         imgLayer=imageFor(url1, lon0, lat0, lon1, lat1, w, h, dpt)
+         imgResult=Image.alpha_composite(imgResult,imgLayer)
+      if url2:
+         imgLayer=imageFor(url2, lon0, lat0, lon1, lat1, w, h, dpt)
+         imgResult=Image.alpha_composite(imgResult,imgLayer)
+      imgResult.save( 'tmp.png', 'PNG' )
 
       self.set_header("Content-type",  "image/png")
       with open('tmp.png','rb') as f: 
