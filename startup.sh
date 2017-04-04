a6fdf54591d67c6515c521a1106c9f5613fee6b4
DuncanRowland <duncan.a.rowland@gmail.com>
2017-04-04 15:45:37 +0100

    Update startup.sh
    Moved iptables config to startup (so only call once)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 cd /home/ubuntu/EasyMap_Shim
-sudo iptables-restore < iptables.conf
+sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 source ENV/bin/activate
 nohup python server.py &
 
