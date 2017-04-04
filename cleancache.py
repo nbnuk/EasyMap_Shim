9f909b3d5eb48f993eb0a2afbf03fceb2e555727
Duncan <duncan.a.rowland@gmail.com>
2017-04-04 07:41:54 +0100

    Added cache cleaner

---
+++ cleancache.py
@@ -0,0 +1,14 @@
+#Daily cronjob to remove 'stale' files from the cache
+
+from glob import glob
+from time import time
+from os.path import getmtime
+from os import remove
+
+old = 31*24*60*60
+now = time()
+cachefiles=glob("cache/*/*/*")
+for filepath in cachefiles:
+   if (now-getmtime(filepath))>old:
+      remove(filepath)      
+
