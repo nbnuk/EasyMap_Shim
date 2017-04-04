6bd4d20c714b2336234cd88b52d0e8342a1ff1c5
DuncanRowland <duncan.a.rowland@gmail.com>
2017-04-04 16:04:22 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
 cd /home/ubuntu/EasyMap_Shim
-sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables-restore < iptables.conf
 source ENV/bin/activate
 nohup python server.py &
 
