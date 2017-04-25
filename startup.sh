5ce4b2d707a22b0f94697796850203f473dad442
Ubuntu <ubuntu@ip-172-31-20-49.eu-central-1.compute.internal>
2017-04-25 11:52:19 +0000

    Turn off nohup logs

--- startup.sh
+++ startup.sh
@@ -3,6 +3,5 @@
 cd /home/ubuntu/EasyMap_Shim
 sudo iptables-restore < iptables.conf
 source ENV/bin/activate
-nohup python -u httpredirectserver.py &
-nohup python -u server.py &
-
+nohup python -u httpredirectserver.py </dev/null >/dev/null 2>&1 &
+nohup python -u server.py </dev/null >/dev/null 2>&1 &
